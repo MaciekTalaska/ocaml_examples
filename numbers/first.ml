119dab6790b6c8083e198709067959b40caa3b48
MaciekTalaska <maciek.talaska@gmail.com>
2019-05-10 21:40:24 +0200

    double semicolon is not always needed

--- numbers/first.ml
+++ numbers/first.ml
@@ -1,21 +1,21 @@
 let average a b =
   let sum = a +. b in
-  sum /. 2.0;;
+  sum /. 2.0
 
 let calc_rest n =
-  n -. float_of_int (truncate n);;
+  n -. float_of_int (truncate n)
 
 let round2 n =
   let r = calc_rest n in
   match r with
   _ when r >= 0.5 -> ceil n
-  | _ -> floor n;;
+  | _ -> floor n
 
 let round n =
   let r = calc_rest n in
   match r >= 0.5 with
   | true -> ceil n
-  | false -> floor n;;
+  | false -> floor n
 
 let inputs = [3.4; 3.5; 3.6];;
 
