a4c97ec34547e806e698dd389cb6b9c899307dd1
MaciekTalaska <maciek.talaska@gmail.com>
2019-05-12 23:51:11 +0200

    type annnotations added to functions

--- numbers/first.ml
+++ numbers/first.ml
@@ -1,17 +1,17 @@
-let average a b =
+let average (a:float) (b:float) : float =
   let sum = a +. b in
   sum /. 2.0
 
-let calc_rest n =
+let calc_rest (n:float) : float =
   n -. float_of_int (truncate n)
 
-let round2 n =
+let round2 (n:float) : float =
   let r = calc_rest n in
   match r with
   _ when r >= 0.5 -> ceil n
   | _ -> floor n
 
-let round n =
+let round (n:float) : float =
   let r = calc_rest n in
   match r >= 0.5 with
   | true -> ceil n
