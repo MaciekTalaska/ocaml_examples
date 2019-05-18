0e305ba142161534d5437527ffd66f66bd36e000
MaciekTalaska <maciek.talaska@gmail.com>
2019-05-18 19:42:14 +0200

    more unit tests

--- numbers/test/first_test.ml
+++ numbers/test/first_test.ml
@@ -15,6 +15,24 @@ let test_fixture = "NumbersTest" >:::
     assert_equal 4.0 (Numbers.round 3.5);
     assert_equal 4.0 (Numbers.round 3.6);
   );
+
+  "round 2" >:: (fun () ->
+    assert_equal 3.0 (Numbers.round2 3.4);
+    assert_equal 4.0 (Numbers.round2 3.5);
+    assert_equal 4.0 (Numbers.round2 3.6);
+  );
+
+  "isEven" >:: (fun () ->
+    assert_equal true (Numbers.isEven 2);
+    assert_equal true (Numbers.isEven 4);
+    assert_equal false (Numbers.isEven 3);
+  );
+
+  "isOdd" >:: (fun () ->
+    assert_equal true (Numbers.isOdd 1);
+    assert_equal true (Numbers.isOdd 3);
+    assert_equal false (Numbers.isOdd 4);
+  );
 ]
 
 let _ = run_test_tt test_fixture
