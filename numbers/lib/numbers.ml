afb4ae8808a1b33e8c569ffbf9b976a17d9510fc
MaciekTalaska <maciek.talaska@gmail.com>
2019-05-19 12:30:31 +0200

    fixing round and round2 (negative input)

--- numbers/lib/numbers.ml
+++ numbers/lib/numbers.ml
@@ -1,3 +1,8 @@
+let abs (a:float) : float =
+  match a >= 0.0 with
+  | true -> a
+  | false -> a *. (-1.0)
+
 let average (a:float) (b:float) : float =
   let sum = a +. b in
   sum /. 2.0
@@ -5,17 +10,25 @@ let average (a:float) (b:float) : float =
 let calc_rest (n:float) : float =
   n -. float_of_int (truncate n)
 
-let round2 (n:float) : float =
+let round2 (i:float) : float =
+  let n = abs i in
   let r = calc_rest n in
-  match r with
-    _ when r >= 0.5 -> ceil n
-  | _ -> floor n
+  let result  = match r with
+  _ when r >= 0.5 -> ceil n
+  | _ -> floor n in
+  match n with
+    _ when i >= 0.0 -> result
+  | _ -> -.result
 
-let round (n:float) : float =
+let round (i:float) : float =
+  let n = abs i in
   let r = calc_rest n in
-  match r >= 0.5 with
+  let result = match r >= 0.5 with
   | true -> ceil n
-  | false -> floor n
+  | false -> floor n in
+  match i >= 0.0 with
+  | true -> result
+  | false -> -.result
 
 let isEven (n:int) : bool =
   n mod 2 == 0
--- numbers/test/first_test.ml
+++ numbers/test/first_test.ml
@@ -3,24 +3,35 @@ open Lib
 
 let test_fixture = "NumbersTest" >:::
 [
-  "testing average" >:: (fun () ->
+  "abs" >:: (fun () ->
+      assert_equal 1.0 (Numbers.abs 1.0);
+      assert_equal 1.0 (Numbers.abs (-1.0));
+  );
+
+  "average" >:: (fun () ->
     assert_equal 4.0 (Numbers.average 3.0 5.0);
     assert_equal 1.0 (Numbers.average 1.0 1.0);
     assert_equal 0.0 (Numbers.average (-1.0) 1.0);
     assert_equal (-4.0) (Numbers.average (-3.0) (-5.0));
-    );
+  );
 
   "round" >:: (fun () ->
     assert_equal 3.0 (Numbers.round 3.4);
     assert_equal 4.0 (Numbers.round 3.5);
     assert_equal 4.0 (Numbers.round 3.6);
-  );
+    assert_equal (-3.0) (Numbers.round (-3.3));
+    assert_equal (-4.0) (Numbers.round (-3.5));
+    assert_equal (-4.0) (Numbers.round (-3.6));
+    );
 
   "round 2" >:: (fun () ->
     assert_equal 3.0 (Numbers.round2 3.4);
     assert_equal 4.0 (Numbers.round2 3.5);
     assert_equal 4.0 (Numbers.round2 3.6);
-  );
+    assert_equal (-3.0) (Numbers.round2 (-3.3));
+    assert_equal (-4.0) (Numbers.round2 (-3.5));
+    assert_equal (-4.0) (Numbers.round2 (-3.6));
+    );
 
   "isEven" >:: (fun () ->
     assert_equal true (Numbers.isEven 0);
