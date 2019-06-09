5afc7060b4b3a6303d6bfce8a85af8b3d93814c1
MaciekTalaska <maciek.talaska@gmail.com>
2019-06-10 00:47:24 +0200

    code extracted to external 'lib'

--- happy_numbers/happy_numbers.ml
+++ happy_numbers/happy_numbers.ml
@@ -1,33 +1,4 @@
-let rec range start stop =
-  if start > stop then []
-  else start :: range (start+1) stop
-
-let intsquare_of_char (c: char) : int =
-  let n = int_of_char c - int_of_char '0' in
-  n * n;;
-
-let intsquare_of_string (s: string) : int =
-  let stop = (String.length s) - 1 in
-  let start = 0 in
-  let temp = range start stop in
-  let squares = List.map (fun x -> intsquare_of_char (String.get s x)) temp in
-  List.fold_left (+) 0 squares;;
-
-let rec isHappyInternal (n: int) (all_numbers: 'int list) (p: bool) : bool =
-  if p then
-    ignore (Printf.printf "%d -> " n);
-  let newn = intsquare_of_string (string_of_int n) in
-  if newn == 1 then
-    true
-  else if List.mem newn all_numbers then
-    false
-  else
-    isHappyInternal newn (newn :: all_numbers) p
-
-let isHappy (n: int) (p: bool) : bool =
-  isHappyInternal n [] p
-
-let inputs = range 1 30;;
+let inputs = Lib.Happy_numbers.range 1 30;;
 
 Printf.printf "Value\tisHappy?\n";;
-List.iter (fun x -> Printf.printf "%d\t%B\n" x (isHappy x true)) inputs;;
+List.iter (fun x -> Printf.printf "%d\t%B\n" x (Lib.Happy_numbers.isHappy x true)) inputs;;
---
+++ happy_numbers/lib/happy_numbers.ml
@@ -0,0 +1,30 @@
+let rec range start stop =
+  if start > stop then []
+  else start :: range (start+1) stop
+
+let intsquare_of_char (c: char) : int =
+  let n = int_of_char c - int_of_char '0' in
+  n * n;;
+
+let intsquare_of_string (s: string) : int =
+  let stop = (String.length s) - 1 in
+  let start = 0 in
+  let temp = range start stop in
+  let squares = List.map (fun x -> intsquare_of_char (String.get s x)) temp in
+  List.fold_left (+) 0 squares;;
+
+let rec isHappyInternal (n: int) (all_numbers: 'int list) (p: bool) : bool =
+  if p then
+    ignore (Printf.printf "%d -> " n);
+  let newn = intsquare_of_string (string_of_int n) in
+  if newn == 1 then
+    true
+  else if List.mem newn all_numbers then
+    false
+  else
+    isHappyInternal newn (newn :: all_numbers) p
+
+let isHappy (n: int) (p: bool) : bool =
+  isHappyInternal n [] p
+
+
